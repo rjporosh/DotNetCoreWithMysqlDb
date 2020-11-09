051b608569f9005354b608d50b409948a3972bc7
Developer Prince <Developer Prince@DESKTOP-L18SEQ6>
2020-11-09 23:30:06 +0600

    Crud Done in MySql And DotNetCore

--- DotNetCoreWithMySql/Controllers/HomeController.cs
+++ DotNetCoreWithMySql/Controllers/HomeController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DotNetCoreWithMySql.Models;
+using DotNetCoreWithMySql.Models.Students;
+using DotNetCoreWithMySql.Models.DatabaseContext;
 
 namespace DotNetCoreWithMySql.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private MyDbContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MyDbContext context)
         {
             _logger = logger;
+            this.context = context;
         }
 
         public IActionResult Index()
@@ -24,7 +28,18 @@ namespace DotNetCoreWithMySql.Controllers
         }
         public IActionResult Student()
         {
-            return View();
+            IEnumerable<Student> model = context.Set<Student>().ToList().Select(s => new Student
+            {
+                Id = s.Id,
+                Name = $"{s.Name} ",
+                Age = s.Age,
+                Gender = s.Gender,
+                Phone = s.Phone,
+                Address = s.Address,
+                Permanent_Address = s.Permanent_Address,
+                E_Mail = s.E_Mail
+            });
+            return View("Student", model);
         }
 
         public IActionResult Privacy()
--- DotNetCoreWithMySql/Controllers/StudentController.cs
+++ DotNetCoreWithMySql/Controllers/StudentController.cs
@@ -38,70 +38,149 @@ namespace DotNetCoreWithMySql.Controllers
         // GET: Student/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Student model = new Student();
+            if (id > 0)
+            {
+                Student student = context.Set<Student>().SingleOrDefault(c => c.Id == id);
+                if (student != null)
+                {
+                    model.Id = student.Id;
+                    model.Name = student.Name;
+                    model.Age = student.Age;
+                    model.Address = student.Address;
+                    model.Permanent_Address = student.Permanent_Address;
+                    model.Gender = student.Gender;
+                    model.Phone = student.Phone;
+                    model.E_Mail = student.E_Mail;
+                }
+            }
+            return View("~/Views/Student/Details.cshtml", model);
         }
-
+        [HttpGet]
         // GET: Student/Create
         public ActionResult Create()
         {
-            return View();
+            Student model = new Student();
+            return View("Create",model);
         }
 
         // POST: Student/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(IFormCollection collection, Student model)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    Student student = new Student();
+                 //   {
+                        // AddedDate = DateTime.UtcNow
+                  //  } : context.Set<Student>().SingleOrDefault(s => s.Id == id);
+                    student.Name = model.Name;
+                    student.Age = model.Age;
+                    student.Address = model.Address;
+                    student.Permanent_Address = model.Permanent_Address;
+                    student.Gender = model.Gender;
+                    student.Phone = model.Phone;
+                    student.E_Mail = model.E_Mail;
+                    // student.IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+                    // student.ModifiedDate = DateTime.UtcNow;
+                   // if (isNew)
+                   // {
+                        context.Add(student);
+                  //  }
+                    context.SaveChanges();
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                throw ex;
             }
+            return RedirectToAction("Index");
         }
-
+        [HttpGet]
         // GET: Student/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Student model = new Student();
+           
+                Student student = context.Set<Student>().SingleOrDefault(c => c.Id == id);
+                if (student != null)
+                {
+                    //model.Id = student.Id;
+                    model.Name = student.Name;
+                    model.Age = student.Age;
+                    model.Address = student.Address;
+                    model.Permanent_Address = student.Permanent_Address;
+                    model.Gender = student.Gender;
+                    model.Phone = student.Phone;
+                    model.E_Mail = student.E_Mail;
+                }
+            
+            return View("~/Views/Student/Edit.cshtml", model);
         }
 
         // POST: Student/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, IFormCollection collection, Student model)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    Student student = context.Set<Student>().SingleOrDefault(c => c.Id == id);
+                   
+
+                 //   Student student = new Student();
+                  // {
+                  //      AddedDate = DateTime.UtcNow
+                  //  } : context.Set<Student>().SingleOrDefault(s => s.Id == id);
+                    student.Name = model.Name;
+                    student.Age = model.Age;
+                    student.Address = model.Address;
+                    student.Permanent_Address = model.Permanent_Address;
+                    student.Gender = model.Gender;
+                    student.Phone = model.Phone;
+                    student.E_Mail = model.E_Mail;
+                    // student.IPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+                    // student.ModifiedDate = DateTime.UtcNow;
+                    //   if (isNew)
+                    //   {
+                    //      context.Add(student);
+                    //  }
+                    context.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    context.SaveChanges();
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                throw ex;
             }
+            return RedirectToAction("Index");
         }
 
         // GET: Student/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Student student = context.Set<Student>().SingleOrDefault(c => c.Id == id);
+            Student model = student;
+          //  {
+           //     Name = $"{student.Name}"
+          //  };
+            return View("~/Views/Student/Delete.cshtml", model);
         }
 
         // POST: Student/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, IFormCollection collection,Student model)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            Student student = context.Set<Student>().SingleOrDefault(c => c.Id == id);
+            context.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
